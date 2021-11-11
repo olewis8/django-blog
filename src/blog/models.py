34c3a52af1fee4810c5618929597374c946e9eb5
Owen <owenlewis902@gmail.com>
2021-11-11 16:16:40 -0500

    added get_absolute_url to BlogPost model

--- src/blog/models.py
+++ src/blog/models.py
@@ -7,3 +7,6 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     content = models.TextField()
+
+    def get_absolute_url(self):
+        return f'/blog/{self.id}'
