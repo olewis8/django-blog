84532dd1f8e197cdace0f155335d9b2bcccd762e
Owen <owenlewis902@gmail.com>
2021-11-11 14:53:29 -0500

    dynamically access posts via url

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,5 +5,5 @@ from .views import (blog_list_view,
 
 urlpatterns = [
     path('', blog_list_view),
-    path('detail', blog_detail_view),
+    path('<int:post_id>/', blog_detail_view),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import BlogPost
 
 
 def blog_list_view(request):
@@ -10,11 +10,13 @@ def blog_list_view(request):
     return render(request, template_name, context)
 
 
-def blog_detail_view(request):
+def blog_detail_view(request, post_id):
+    blog_post = get_object_or_404(BlogPost, id=post_id)
+
     template_name = 'pages/blog_detail.html'
-    context = {'title': 'blog post',
-               'author': 'author',
-               'content': 'content goes here'
+    context = {'title': blog_post.title,
+               'author': blog_post.author,
+               'content': blog_post.content,
                }
 
     return render(request, template_name, context)
