745be8f850711883dfffc224787e3f5d93a78e2d
Owen <owenlewis902@gmail.com>
2021-11-18 14:35:05 -0500

    added name to login url pattern

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (login_view, logout_view, register_view)
 
 urlpatterns = [
-    path('login/', login_view),
+    path('login/', login_view, name='login'),
     path('logout/', logout_view),
     path('register/', register_view),
 ]
