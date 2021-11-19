9971b2ba0333fe448944e9dad42194fcb697e213
Owen <owenlewis902@gmail.com>
2021-11-18 21:05:10 -0500

    added create/update signals for Profile model

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -7,5 +9,16 @@ class Profile(models.Model):
     bio = models.TextField(max_length=50, blank=True)
     location = models.CharField(max_length=30, blank=True)
 
-    def get_profile_absolute_url(self):
+    def get_profile_url(self):
         return f'/users/{self.user.username}'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
