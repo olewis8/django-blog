60aafdd86c71b74fe2c143c3fe2192813c623ecc
Owen <owenlewis902@gmail.com>
2021-11-18 22:51:15 -0500

    fixed bug

--- src/users/views.py
+++ src/users/views.py
@@ -66,6 +66,8 @@ def profile_view(request, username):
     user = get_object_or_404(User, username=username)
 
     template_name = 'pages/profile.html'
-    context = {'user': user}
+    context = {'username': user.username,
+               'location': user.profile.location,
+               'bio': user.profile.bio, }
 
     return render(request, template_name, context)
