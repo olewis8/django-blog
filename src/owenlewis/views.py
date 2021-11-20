8a95bc83ff3a070d135d7ac3b1650ff852ed5c03
Owen <owenlewis902@gmail.com>
2021-11-20 14:41:22 -0500

    added non-blog non-form pages to card view

--- src/owenlewis/views.py
+++ src/owenlewis/views.py
@@ -4,17 +4,18 @@ from .forms import ContactForm
 
 
 def home_view(request):
-    template_name = 'pages/home.html'
+    template_name = 'pages/card.html'
     context = {'title': 'hello world,,,',
-               'message': '',
+               'body': 'welcome to my website',
                }
 
     return render(request, template_name, context)
 
 
 def about_view(request):
-    template_name = 'pages/about.html'
+    template_name = 'pages/card.html'
     context = {'title': 'about me',
+               'body': 'i\'m owen bla bla bla'
                }
 
     return render(request, template_name, context)
