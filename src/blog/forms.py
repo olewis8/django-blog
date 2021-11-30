001f559ad2ca02256efc352394a4e090ce8e1c36
Owen <owenlewis902@gmail.com>
2021-11-30 16:40:52 -0500

    added date fields to blog post model

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -10,12 +10,18 @@ class CreateBlogPost(forms.ModelForm):
 
     def save(self, commit=True, **kwargs):
         author = None
+        modified = None
+
         if 'author' in kwargs:
             author = kwargs.pop('author')
+        if 'modified' in kwargs:
+            modified = kwargs.pop('modified')
 
         m = super(CreateBlogPost, self).save(commit=False, **kwargs)
 
         if m.author == '' and author is not None:
             m.author = author
+        if modified is not None:
+            m.modified = modified
 
         m.save()
--- src/blog/models.py
+++ src/blog/models.py
@@ -1,17 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     content = models.TextField()
+    created = models.DateTimeField(default=timezone.now, editable=False)
+    modified = models.DateTimeField(default=timezone.now, editable=True)
 
     def get_absolute_url(self):
         return f'/blog/{self.id}'
-
-    # def save(self, **kwargs):
-    #
-    #     if 'author' in kwargs and self.author is None:
-    #         self.author = kwargs.pop('author')
-    #
-    #     super(BlogPost, self).save(**kwargs)
