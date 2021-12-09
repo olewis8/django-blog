3052a2409c4146d9b4ee4ed64c32b2eac1e63ba7
Owen <owenlewis902@gmail.com>
2021-12-09 17:19:18 -0500

    replaced new comment modelform with new comment form

--- src/comments/forms.py
+++ src/comments/forms.py
@@ -3,20 +3,5 @@ from django import forms
 from .models import (Comment)
 
 
-class CreateComment(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['text']
-
-    def save(self, commit=True, **kwargs):
-        user = None
-
-        if 'user' in kwargs:
-            user = kwargs.pop('user')
-
-        m = super(CreateComment, self).save(commit=False, **kwargs)
-
-        if m.user == '' and user is not None:
-            m.user = user
-
-        m.save()
+class CreateComment(forms.Form):
+    text = forms.CharField(widget=forms.Textarea())
