0de3302a1ea53ac771017bb899603adf50d75375
Owen <owenlewis902@gmail.com>
2022-01-24 16:43:01 -0500

    remove unused import

--- src/comments/urls.py
+++ src/comments/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import (comment_page,
-                    retrieve_comments,
+from .views import (retrieve_comments,
                     create_comment,
                     delete_comment)
 
