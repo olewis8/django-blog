e3f1b0f5081b488186f44de123b510bfce8a1de7
Owen <owenlewis902@gmail.com>
2022-03-12 13:32:54 -0500

    rename postElement to postListElement

--- src/blog/static/blog/load-post-previews.js
+++ src/blog/static/blog/load-post-previews.js
@@ -1,10 +1,10 @@
 import { loadPostPreviews } from './module.js'
 
-const postElement = document.getElementById('posts')
+const postListElement = document.getElementById('posts')
 
 if (localStorage.getItem('buttonClicked') == 'disc'){
-  loadPostPreviews(postElement, 'disc')
+  loadPostPreviews(postListElement, 'disc')
   localStorage.removeItem('buttonClicked')
 }
 
-loadPostPreviews(postElement, 'fy')
+loadPostPreviews(postListElement, 'fy')
