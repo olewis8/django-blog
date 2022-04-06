dd97a845b6c808db742459d62f6bff5b598854d5
Owen <owenlewis902@gmail.com>
2022-04-06 14:26:01 -0400

    use django inheritance for page specific css

--- src/owenlewis/urls.py
+++ src/owenlewis/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import (home_view,)
+from .views import (home_view, projects_page)
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('projects/', projects_page),
     path('admin/', admin.site.urls),
 
     path('blog/', include('blog.urls')),
--- src/owenlewis/views.py
+++ src/owenlewis/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 def home_view(request):
     template_name = 'pages/home.html'
-    context = {}
+    return render(request, template_name)
 
-    return render(request, template_name, context)
+
+def projects_page(request):
+    template_name = 'pages/projects.html'
+    return render(request, template_name)
