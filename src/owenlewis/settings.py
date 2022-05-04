028dffd00be67061268bd617467f517eaeddf681
Owen <owenlewis902@gmail.com>
2022-05-04 15:41:41 -0400

    update allowed hosts

--- src/owenlewis/settings.py
+++ src/owenlewis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['infinite-mountain-24270.herokuapp.com', 'owenlewis.ca', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['infinite-mountain-24270.herokuapp.com', 'owenlewis.ca', 'www.owenlewis.ca', '127.0.0.1', '0.0.0.0']
 
 LOGIN_URL = '/users/login'
 
